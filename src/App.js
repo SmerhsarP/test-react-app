343aa7b98fbddec93fc827cfc9a464f1640397bd
SmerhsarP <SmerhsarP@gmail.com>
2024-03-03 13:33:43 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
-function App() {
+const getPeopleData = (event) => {
+  const url = 'https://swapi.dev/api/people';
+
+  return fetch(url, { method: 'GET' })
+    .then(response => response.json());
+}
+
+const tableRendering = (data) => {
+  if (data.length === 0) {
+    return (<tr><td colSpan={5}>Загрузите данные</td></tr>)
+  }
+
+  return data.map(row => (
+    <tr>
+      <td>{row.name}</td>
+      <td>{row.gender}</td>
+      <td>{row.hair_color}</td>
+      <td>{row.height}</td>
+      <td>{row.eye_color}</td>
+    </tr>
+  ));
+}
+
+const App = () => {
+  const [tableData, setTableData] = useState([]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div class="buttons-container">
+        <button onClick={(event) => getPeopleData().then(data => setTableData(data.results))}>Сделать запрос</button>
+        <button onClick={(event) => setTableData([])}>Очистить таблицу</button>
+      </div>
+
+      <table class="people-table">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Gender</th>
+            <th>Hair Color</th>
+            <th>Height</th>
+            <th>Eye Color</th>
+          </tr>
+        </thead>
+        <tbody>
+          {tableRendering(tableData)}
+        </tbody>
+      </table>
     </div>
   );
 }
